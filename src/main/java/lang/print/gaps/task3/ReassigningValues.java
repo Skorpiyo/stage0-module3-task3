b10cff9900c7114e0242b5e3785cd6e18766b734
Skorpiyo <skorpio277@gmail,com>
2022-09-16 22:45:24 +0300

    Basic: create first, second, third and initialize them Declaring: a = b = c = 10 Reassigning:  write to console all variables

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -2,6 +2,27 @@ package lang.print.gaps.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
+        int first;
+        int second;
+        int third;
+        first = 1;
+        second = 10;
+        third = 100;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        first = 15;
+        second = 6;
+        third = 4;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        first = 1;
+        second = 10;
+        third = 100;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
         //write code here
     }
 }
